9185b3d04283663762c604f734c4dcde8db9f04a
Omelaa <ostpa28@gmail.com>
2021-08-17 21:37:35 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+
+new Swiper('.info-slider__items',{
+  slidesPerView:6,
+  // centeredSlides: true,
+  loop: true,
+
+   autoplay: {
+    // Пауза между прокруткой
+		delay: 2000,
+		// Закончить на последнем слайде
+		stopOnLastSlide: false,
+		// Отключить после ручного переключения
+		disableOnInteraction: false
+	},
+  speed: 300,
+})
+
+
+
+$(function(){
+
+          
+});
